87a32a4d8f00bc38244da9bee4eba889b27f7429
phunami <phunami@phunguye-mn1.(none)>
2013-07-13 05:18:50 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DicisIo::Application.config.secret_token = '247be7f5d1ad0c0798dcc9ac79dc99ca4136c98a8f741364679f19b2a260642a053fbc95f86cf1aa329f90cb5942c380fd0968bcfb514d599b2323b5bf21b123'
